e7d9dbc090288647d02f8dbf0a1e6b7c165e5f01
Fenyuan666 <fen887150@gmail.com>
2025-08-23 18:50:06 +0800

    更新开场白

--- try.py
+++ try.py
@@ -1,7 +1,14 @@
-x = 5
-def func():
-    global x
-    print(x)
-    x = 10
-    print(x)
-func()
+import tracemalloc
+
+tracemalloc.start()
+
+# 模拟内存分配
+data = [list(range(1000)) for _ in range(100)]
+
+# 拍照
+snapshot = tracemalloc.take_snapshot()
+top_stats = snapshot.statistics('lineno')  # 按行号统计
+
+print("内存占用前5名:")
+for stat in top_stats[:5]:
+    print(stat)
